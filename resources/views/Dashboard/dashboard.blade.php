0fb68c5d99ec2de099e96e30c00047cec94633cf
Hendra1177 <ahendra604@gmail.com>
2022-07-13 21:47:30 +0700

    frontend

---
+++ resources/views/Dashboard/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends ('layouts.master')
+@section('content')
+<main class="main-content position-relative border-radius-lg ps">
+    <div class="container-fluid py-4">
+         <h1 class="my-6 text-6x5 font-bold text-white text-center">
+            SELAMAT DATANG DI KEMITRAAN BPS JOMBANG
+         </h1>
+         
+    </div>
+</main>
+   
+  @endsection  
